cd2a1baddb2cb54ab72231fe8496d2ad47aaf2e5
CalebArndt <supernerd1226@gmail.com>
2020-07-10 20:13:19 -0400

    made basic visuals for rooms and fire levels

--- room.py
+++ room.py
@@ -1,3 +1,5 @@
+import pygame
+import ship
 class Room:
     def __init__(self, x, y, adjacent): # x and y are the canvas coordinates, used for drawing
         self.x = x
@@ -5,9 +7,19 @@ class Room:
         self.adjacent = adjacent # this is an array of integers
 
         self.fireLevel = 0
-        self.spreadChance = 0.4 # chance that an adjacent level 2 fire spreads to this room
-        self.growthChance = 0.4 # chance that a level 1 fire grows to level 2
+        self.spreadChance = 0.2 # chance that an adjacent level 2 fire spreads to this room
 
         self.sprinkling = False
 
+    def draw_room(self, surface): # will be replaced once we have room visuals
+        if self.fireLevel == 0:
+            colour = (0, 255, 0)
+        elif self.fireLevel == 1:
+            colour = (255, 150, 0)
+        else:
+            colour = (255, 0, 0)
+        pygame.draw.rect(surface, colour, (self.x, self.y, 50, 50))
+        for i in self.adjacent:
+            end_pos = (ship.lookUp(i).x + 25, ship.lookUp(i).y + 25)
+            pygame.draw.line(surface, (255, 255, 255), (self.x + 25, self.y + 25), end_pos, 4)
 
--- ship.py
+++ ship.py
@@ -1,6 +1,21 @@
 import random
+import room
 
 roomArray = [] # to be filled with room objects
+tempRoom = room.Room(50, 50, [1, 3])
+roomArray.append(tempRoom)
+tempRoom = room.Room(150, 50, [0, 4])
+roomArray.append(tempRoom)
+tempRoom = room.Room(250, 50, [5])
+roomArray.append(tempRoom)
+tempRoom = room.Room(50, 150, [0, 4])
+roomArray.append(tempRoom)
+tempRoom = room.Room(150, 150, [1, 3])
+roomArray.append(tempRoom)
+tempRoom = room.Room(250, 150, [2])
+roomArray.append(tempRoom)
+
+roomArray[0].fireLevel = 1
 
 def fireTick():
     for i in roomArray:
@@ -12,8 +27,7 @@ def fireTick():
                         break
                 
         elif i.fireLevel == 1:
-            if random.random() <= i.growthChance:
-                i.fireLevel = 2
+            i.fireLevel = 2
     return
 
 def lookUp(index):
